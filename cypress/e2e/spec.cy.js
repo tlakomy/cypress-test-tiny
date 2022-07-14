ebd8024b87db7998a8c6fb5012cbd00fc3efccbb
Tomasz Łakomy <tomasz@stedi.com>
2022-07-14 12:43:03 +0200

    chore: prepare a minimal repro example

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,11 @@
 /// <reference types="cypress" />
+
+import isEven from 'is-even'
+
 describe('page', () => {
   it('works', () => {
     cy.visit('https://example.cypress.io')
+
+    expect(isEven(2)).to.be.true;
   })
 })
